9ee0c10a9ee6fdf2c9e0382d27509db602d008c0
Fernando Castillo <superferenabled@gmail.com>
2026-01-20 22:05:31 -0600

    Remove margin from cards, increase padding from p-4.5 to p-6

--- src/components/ui/Card.jsx
+++ src/components/ui/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Card({ title, children, className = "" }) {
     return (
-        <article className={`bg-f-card p-4.5 rounded-xl flex-1 ${className}`}>
+        <article className={`bg-f-card p-6 rounded-xl flex-1 ${className}`}>
             {title && <h3 className="text-lg font-semibold mb-2">{title}</h3>}
             <div>{children}</div>
         </article>
